8c2f8ddd9f6d553bb1e8a2ff07374709f9247850
dtran <tdthuan97@gmail.com>
2018-08-07 16:58:47 +0700

    last

---
+++ tchou_tchou/tchou_tchou.py
@@ -0,0 +1,19 @@
+Write a program tchou_tchou.py that prints a train of various sizes.
+
+$ python3 tchou_tchou.py
+What is the size of your train? 0
+$ python3 tchou_tchou.py
+What is the size of your train? 1
+ ______________________>__
+|]||[]_[]_[]|||[]_[]_[]||[|
+\==o-o======o-o======o-o==/
+$ python3 tchou_tchou.py
+What is the size of your train? 2
+ _________________________   ______________________>__
+|]||[]_[]_[]|||[]_[]_[]||[| |]||[]_[]_[]|||[]_[]_[]||[|
+\==o-o======o-o======o-o==/_\==o-o======o-o======o-o==/
+$ python3 tchou_tchou.py
+What is the size of your train? -1
+Don't be so negative.
+
+
